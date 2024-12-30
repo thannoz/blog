151cf5fde6d8dc56b9c40aa7bb16589eeb24b101
thannoz <carlos-7@hotmail.de>
2024-12-30 16:36:27 +0100

    ui: Finish sign in and sign up ui implementation

--- client/src/pages/authForm.tsx
+++ client/src/pages/authForm.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import InputBox from "../components/InputBox";
+import googleIcon from "../img/google.png";
 
 interface AuthFormProps {
   authType: string;
@@ -31,6 +33,40 @@ const AuthForm = ({ authType }: AuthFormProps) => {
           placeholder="Password"
           icon="fi-rr-key"
         />
+
+        <button className="btn-dark center mt-14">
+          {authType.replace("-", " ")}
+        </button>
+
+        <div
+          className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase
+        text-black font-bold"
+        >
+          <hr className="w-1/2 border-black" />
+          <p>or</p>
+          <hr className="w-1/2 border-black" />
+        </div>
+
+        <button className="btn-dark flex items-center justify-center gap-2 w-[90&] center">
+          <img src={googleIcon} alt="google icon" className="w-5" />
+          continue with google
+        </button>
+
+        {authType === "sign-in" ? (
+          <p className="text-center mt-6 text-dark-grey text-xl">
+            Don't have an account?
+            <Link to="/signup" className="underline text-black text-xl ml-1">
+              Join us today
+            </Link>
+          </p>
+        ) : (
+          <p className="text-center mt-6 text-dark-grey text-xl">
+            Already have an account?
+            <Link to="/signin" className="underline text-black text-xl ml-1">
+              Sign in here.
+            </Link>
+          </p>
+        )}
       </form>
     </section>
   );
